fa3c2b0019df7f200579e5d2edb3aa0528bd9c54
rito.ishihara <rito.ishihara@optim.co.jp>
2023-12-07 02:30:01 +0900

    style: eslint, prettierの実行

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
-  webpack:(config, options) => {
+  webpack: (config, options) => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     require('./webpack.config')(config, options);
     return config;
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-
-
 module.exports = (config) => {
   // Add your custom Dev Middleware configuration here
   config.watchOptions = {
