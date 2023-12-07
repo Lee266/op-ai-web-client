714e69c06f376df7dbefb8446ee08a1fbdffd930
rito.ishihara <rito.ishihara@optim.co.jp>
2023-12-07 18:36:48 +0900

    ci: lighthouse-ciのgithub Actionを作成

---
+++ .lighthouserc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  ci: {
+    collect: {
+      // collect options here
+    },
+    assert: {
+      // assert options here
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+    server: {
+      // server options here
+    },
+    wizard: {
+      // wizard options here
+    },
+  },
+};
