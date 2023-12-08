e8028e78623dd5e039d307eb28e026502f0111c1
rito.ishihara <rito.ishihara@optim.co.jp>
2023-12-09 00:40:50 +0900

    feat: pwaの設定を追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
+  reactStrictMode: true,
   webpack: (config, options) => {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     require('./webpack.config')(config, options);
@@ -8,4 +8,18 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  // disable: process.env.NODE_ENV === 'development',
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+});
+
+module.exports = withPWA(nextConfig);
