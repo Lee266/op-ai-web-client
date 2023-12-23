429cfd0b7a18fa75f17f26ecd395b77a134f6c90
rito.ishihara <rito.ishihara@optim.co.jp>
2023-12-24 02:42:42 +0900

    refactor: 開発時にpwaのdisableをtrueにする

--- next.config.js
+++ next.config.js
@@ -8,14 +8,12 @@ const nextConfig = {
   },
 };
 
-// module.exports = nextConfig;
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  // disable: process.env.NODE_ENV === 'development',
+  disable: process.env.MODE === 'development',
   // register: true,
   // scope: '/app',
   // sw: 'service-worker.js',
