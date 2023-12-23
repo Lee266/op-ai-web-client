362fa2470fa4779bea90d21b2f0cec1563830662
rito.ishihara <rito.ishihara@optim.co.jp>
2023-12-24 02:30:32 +0900

    feat: dashboardページを作成

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next';
+import MainLayout from '@/app/components/layouts/MainLayout';
+
+const Dashboard: NextPage = () => {
+  return <MainLayout>Dashboard</MainLayout>;
+};
+
+export default Dashboard;
