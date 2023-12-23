5e52d3de355d2279d5d2404bf94915ee5e363686
rito.ishihara <rito.ishihara@optim.co.jp>
2023-12-24 02:17:58 +0900

    build[npm]: Storybookの追加プラグインとtestを追加

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,25 @@
 import type { Preview } from '@storybook/react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/material-icons';
 import '../src/app/globals.css';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { lightTheme, darkTheme } from '../src/app/theme';
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    themes: {
+      light: lightTheme,
+      dark: darkTheme,
+    },
+    defaultTheme: 'light',
+    Provider: ThemeProvider,
+    GlobalStyles: CssBaseline,
+  }),
+];
 
 const preview: Preview = {
   parameters: {
