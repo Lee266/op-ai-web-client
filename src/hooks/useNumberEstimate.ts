cac98303eddc049c1d1ae8a349e81dbefae7fcba
rito.ishihara <rito.ishihara@optim.co.jp>
2024-01-01 20:49:57 +0900

    feat: 数値画像を推論するapiとhookを作成

---
+++ src/hooks/useNumberEstimate.ts
@@ -0,0 +1,27 @@
+import useSWRMutation from 'swr/mutation';
+
+export function useNumberEstimate() {
+  async function sendRequest(url: string, { arg }: { arg: { image: Blob; model: string } }) {
+    const formData = new FormData();
+    formData.append('image', arg.image);
+
+    return fetch(`${url}?model=${arg.model}`, {
+      method: 'POST',
+      body: formData,
+    })
+      .then(async (res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .catch((e) => {
+        console.error('Error sending request:', e);
+        return { error: e.message };
+      });
+  }
+
+  const { trigger, data, error, isMutating } = useSWRMutation('/api/ai/mnist', sendRequest);
+
+  return { trigger, data, error, isMutating };
+}
